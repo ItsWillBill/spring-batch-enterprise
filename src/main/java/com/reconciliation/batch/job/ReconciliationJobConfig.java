7737ee1a95518665d616a719ccb510dc52dfbc14
billwilliams <tsofackwilliams@gmail.com>
2026-03-12 22:38:30 +0100

    new trial fix CI

--- src/main/java/com/reconciliation/batch/job/ReconciliationJobConfig.java
+++ src/main/java/com/reconciliation/batch/job/ReconciliationJobConfig.java
@@ -3,10 +3,8 @@ package com.reconciliation.batch.job;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
